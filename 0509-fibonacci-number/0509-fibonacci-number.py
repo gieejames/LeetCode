3343c1849e88df16bf732f3499caf2bde29710b5
James Seo <55122004+gieejames@users.noreply.github.com>
2023-03-27 23:36:34 -0500

    Time: 31 ms (75.48%), Space: 13.9 MB (41.91%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,18 +1,28 @@
-memo = [-1]*31
+#memo = [-1]*31
 class Solution:
     
     def fib(self, n: int) -> int:
         #recursive dp 
         #base cases
-        if n == 0: 
+        #if n == 0: 
+        #    return 0
+        #if n == 1:
+        #    return 1
+        
+        #if memo[n] != -1:
+        #    return memo[n]
+        
+        #memo[n] = self.fib(n-1) + self.fib(n-2)
+        #return memo[n]
+        if n == 0:
             return 0
-        if n == 1:
+        if n == 2 or n == 1:
             return 1
-        
-        if memo[n] != -1:
-            return memo[n]
-        
-        memo[n] = self.fib(n-1) + self.fib(n-2)
-        return memo[n]
-        
         #iterative dp 
+        dp = [0]*(n+1)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+        
